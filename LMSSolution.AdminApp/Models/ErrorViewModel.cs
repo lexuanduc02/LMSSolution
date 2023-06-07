3b1cf9c627d4fcac961e0694a00509e9d6a80933
Lê Xuân Đức <lexuanduc.hn@gmail.com>
2023-06-07 20:56:02 +0700

    Add project files.

---
+++ LMSSolution.AdminApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LMSSolution.AdminApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
